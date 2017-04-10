3381f70cd8ba4502f504e7a9ae681bbaef7bb835
Collins Mwiti <collotush@gmail.com>
2017-04-10 12:25:21 +0300

    populating and commenting model admin.js populating and commenting on route index.js

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
+//route for the objects
 export default Ember.Route.extend({
+  contact_name: DS.attr(),
+  contact_no: DS.attr()
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
+//model-hook to fetch objects from route admin.js
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('admin');
+  },
 });

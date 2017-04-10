bbb4c5ede6a441735d4b7ec38485133968e6076b
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-04-10 14:33:59 +0300

    adding functionalities to make appropriate action on the route handle

--- app/components/admin-tile.js
+++ app/components/admin-tile.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
 
-//delete functionality for button delete
 export default Ember.Component.extend({
-  actions: {
-    delete(admin) {
-      if (confirm('Are you sure you want to delete this contact?')) {
-        this.sendAction('destroyAdmin', admin);
-      }
-    }
-  }
+
 });

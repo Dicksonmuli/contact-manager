7067208feee770fa02bc8ab1e0956a4351a9fbad
Collins Mwiti <collotush@gmail.com>
2017-04-10 14:24:52 +0300

    populating and commenting on admin-tile.js

--- app/components/admin-tile.js
+++ app/components/admin-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
+//delete functionality for button delete
 export default Ember.Component.extend({
+  actions: {
+    delete(admin) {
+      if (confirm('Are you sure you want to delete this contact?')) {
+        this.sendAction('destroyAdmin', admin);
+      }
+    }
+  }
 });

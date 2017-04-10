9f337d5af327ca3275759efc86a0a10328f1732b
Collins Mwiti <collotush@gmail.com>
2017-04-10 16:29:25 +0300

    populating and commenting on update-admin.js

--- app/components/update-admin.js
+++ app/components/update-admin.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
+//actions for update-admin.hbs where it is receiving admin as an argument
 export default Ember.Component.extend({
+  updateAdminForm: false,
+  actions: {
+    updateAdminForm() {
+      this.set('updateAdminForm', true);
+    },
+    update(admin) {
+      var params = {
+        contact_name: this.get('contact_name'),
+        contact_no: this.get('contact_no'),
+      };
+      this.set('updateAdminForm', false);
+      this.sendAction('update', admin, params);
+    }
+  }
 });

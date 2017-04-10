d705e845c193a88e754869e9f7e1e6fff2ceb85b
Collins Mwiti <collotush@gmail.com>
2017-04-10 15:39:32 +0300

    populating and commenting on new-admin.js

--- app/components/new-admin.js
+++ app/components/new-admin.js
@@ -6,6 +6,15 @@ export default Ember.Component.extend({
   actions: {
     adminFormShow() {
       this.set('addNewAdmin', true);
+    },
+    // adding save behavior of new-admin.hbs
+    saveAdmin1() {
+      var params = {
+        contact_name: this.get('contact_name'),
+        contact_no: this.get('contact_no'),
+      };
+      this.set('addNewAdmin', false);
+      this.sendAction('saveAdmin2', params);
     }
   }
 });

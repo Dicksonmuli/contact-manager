06e5b2b2505657109243e43d12325cfa4073939e
Collins Mwiti <collotush@gmail.com>
2017-04-10 16:35:24 +0300

    adding update functionality in admin-tile.js

--- app/components/admin-tile.js
+++ app/components/admin-tile.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
-//functionality for button delete of admin-tile.hbs
+//functionality for button delete and also for update for component admin-tile.hbs
 export default Ember.Component.extend({
   actions: {
+    update(admin, params) {
+      this.sendAction('update', admin, params);
+    },
     delete(admin) {
       if (confirm('Are you sure you want to delete this contact?')) {
         this.sendAction('destroyAdmin', admin);

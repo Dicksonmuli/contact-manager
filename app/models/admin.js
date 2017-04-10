de4bf3e314fa207f5614127801f996b63ed35596
Collins Mwiti <collotush@gmail.com>
2017-04-10 12:33:41 +0300

    populating model admin.js

--- app/models/admin.js
+++ app/models/admin.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  contact_name: DS.attr(),
+  contact_no: DS.attr()
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,6 +2,5 @@ import Ember from 'ember';
 
 //route for the objects
 export default Ember.Route.extend({
-  contact_name: DS.attr(),
-  contact_no: DS.attr()
+
 });

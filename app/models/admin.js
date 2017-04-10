d1bba79f5c362d32d59102b2ec2002a6a93b8f22
Collins Mwiti <collotush@gmail.com>
2017-04-10 12:35:32 +0300

    commenting on model admin.js

--- app/models/admin.js
+++ app/models/admin.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
+//route for the objects
 export default DS.Model.extend({
   contact_name: DS.attr(),
   contact_no: DS.attr()
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-//route for the objects
+
 export default Ember.Route.extend({
 
 });

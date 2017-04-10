544568c9919b4a49918c321e782e203cb7b338b5
Collins Mwiti <collotush@gmail.com>
2017-04-10 15:15:03 +0300

    populating and commenting on new-admin.js

--- app/components/new-admin.js
+++ app/components/new-admin.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
+//adding behavior for the button
 export default Ember.Component.extend({
+  addNewAdmin: false,
+  actions: {
+    adminFormShow() {
+      this.set('addNewAdmin', true);
+    }
+  }
 });

1a86abbfc79dbaaaaf18b46a02a18a18779f808a
Collins Mwiti <collotush@gmail.com>
2017-04-10 15:44:32 +0300

    commenting and populating route index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,6 +7,12 @@ export default Ember.Route.extend({
   },
   //creating an action on our route handler
   actions: {
+    saveAdmin3(params) {
+      var newAdmin = this.store.createRecord('admin', params);
+      newAdmin.save();
+      this.transitionTo('index');
+    },
+
     destroyAdmin(admin) {
       admin.destroyRecord();
       this.transitionTo('index');

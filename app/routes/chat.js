415191a46ef986f01b210eb2d7380a3e04ed8fa1
dicksonmuli <dicksonmully6@gmail.com>
2017-04-11 15:02:59 +0300

    adding chat functions including save, new message, reply

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	// actions
+	actions: {
+		saveMessage(params){
+			var newMessage = this.store.createRecord('message', params);
+			newMessage.save();
+			this.transitionTo('chat');
+		},
+		saveReply(params){
+			var newReply = this.store.createRecord('reply', params);
+			newReply.save();
+			this.transitionTo('chat');
+		}
+	}
 });

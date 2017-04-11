817d0785bcbdcaf157e582ae3d8f3d46108565e1
dicksonmuli <dicksonmully6@gmail.com>
2017-04-11 16:41:05 +0300

    new message forms and actions

--- app/components/chat-tile.js
+++ app/components/chat-tile.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	replyShowing: false;
+
+	// actions
+	actions: {
+		reply() {
+			this.set('replyShowing', true);
+		},
+		saveReply(){
+			var params = {
+				content: this.get('content'),
+				file: this.get('file'),
+				message: this.get('message')
+			}
+			this.set('replyShowing', false)
+			this.sendAction('saveReply', message, params)
+		}
+	}
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 //model-hook to fetch objects from route admin.js
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('contact');
-  }
+    return Ember.RSVP.hash({
+    contacts: this.store.findAll('rental'),
+    messages: this.store.findAll('review'),
+    replys: this.store.findAll('review')
+  });
+  },
 });

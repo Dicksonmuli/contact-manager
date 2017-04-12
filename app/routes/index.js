53ffdc572cbb715d228d4941003e7a815100bcf0
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-04-12 18:36:07 +0300

    dleted contents

--- app/routes/index.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-//model-hook to fetch objects from route admin.js
-export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('contact');
-  }
-});

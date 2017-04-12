75433812adde180a4ae2725d8f0637a37581f864
Collins Mwiti <collotush@gmail.com>
2017-04-12 19:29:08 +0300

    populating admin.js

---
+++ app/routes/admin.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('contact');
+  },
+  //creating an action on our route handler
+  actions: {
+    saveContact3(params) {
+      var newContact = this.store.createRecord('contact', params);
+      newContact.save();
+      this.transitionTo('admin');
+    },
+
+    update(contact, params) {
+      Object.keys(params).forEach(function(key) {
+        if (params[key] !== undefined) {
+          contact.set(key, params[key]);
+        }
+      });
+      contact.save();
+      this.transitionTo('admin');
+    },
+
+    destroyContact(contact) {
+      contact.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
